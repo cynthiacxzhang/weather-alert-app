42d3649534ef5fa529e634d88754886d69581478
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2025-03-02 18:08:54 -0500

    init

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("Backend is working!");
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
