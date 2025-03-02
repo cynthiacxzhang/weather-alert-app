f217dffd7a49a80fdc58c4ce7ede044da3aeac51
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2025-03-02 18:29:39 -0500

    real-time weather data fetch setup

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 // Middleware
 app.use(express.json());
@@ -18,3 +18,5 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
