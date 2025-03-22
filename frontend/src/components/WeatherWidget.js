365ced791882e9168d233f1fcafe15e3a90c9fc3
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2025-03-21 23:43:15 -0400

    member function 1

--- frontend/src/components/WeatherWidget.js
+++ frontend/src/components/WeatherWidget.js
@@ -5,8 +5,14 @@ import React, { useEffect, useState } from "react";
 
 // Create weatherwidget class
 const WeatherWidget = () => {
+
+    // Member variables
     const [weather, setWeather] = useState(null);   // State to store weather data from OpenWeather API
     const [alert, setAlert] = useState(null);       // State to store alert messages (e.g., extreme weather)
     const [error, setError] = useState(null);       // State to store any error messages (e.g., location access denied)
+
+    // Member functions
+    const fetchWeather = async (lat, lon) => {
+    }
 };
 
