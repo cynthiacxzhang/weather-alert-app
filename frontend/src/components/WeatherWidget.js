c96225220874e153666b58198c6796c14e9be031
cynthiacxzhang <cynthia.zhang.2016@outlook.com>
2025-03-21 23:30:03 -0400

    created widget params

--- frontend/src/components/WeatherWidget.js
+++ frontend/src/components/WeatherWidget.js
@@ -1,3 +1,12 @@
 // File created using "touch src/components/'filename'"
+// First time using js in awhile, take it as a learning process
 
 import React, { useEffect, useState } from "react";
+
+// Create weatherwidget class
+const WeatherWidget = () => {
+    const [weather, setWeather] = useState(null);   // State to store weather data from OpenWeather API
+    const [alert, setAlert] = useState(null);
+    const [error, setError] = useState(null);
+};
+
